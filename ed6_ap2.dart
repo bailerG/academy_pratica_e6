f37ae61c71ccedce3511446dfef647bd2322849c
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-30 14:21:19 -0300

    AP2 Concluida

--- ed6_ap2.dart
+++ ed6_ap2.dart
@@ -1,1 +1,14 @@
-void main() {}
+void main() {
+  try {
+    verificaPar(5);
+    print("Entrada correta, voce inseriu um numero par.");
+  } on Exception catch (e) {
+    print(e);
+  }
+}
+
+void verificaPar(int num) {
+  if (num.isOdd) {
+    throw Exception("Entrada invalida. Insira apenas numeros pares.");
+  }
+}
