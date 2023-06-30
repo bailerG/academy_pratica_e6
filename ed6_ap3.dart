4f97c56561fd728a8d8b45def533e15557c23b36
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-30 14:39:43 -0300

    AP3 Concluida

--- ed6_ap3.dart
+++ ed6_ap3.dart
@@ -1,1 +1,36 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random();
+
+  try {
+    final retangulo =
+        Retangulo(aleatorio.nextDouble() * 99, aleatorio.nextDouble() * 99);
+
+    final double area = retangulo.calcularArea();
+    print("Area do Retângulo: ${area.toStringAsFixed(2)}");
+  } on Exception catch (e) {
+    print(e);
+  }
+}
+
+abstract class Forma {
+  double calcularArea();
+}
+
+class Retangulo implements Forma {
+  Retangulo(this.base, this.altura) {
+    if (base <= 0 || altura <= 0) {
+      throw Exception(
+          "Dimensoes invalidas, informe apenas valores positivos maiores que zero");
+    }
+  }
+
+  final double base;
+  final double altura;
+
+  @override
+  double calcularArea() {
+    return base * altura;
+  }
+}
