8d691bec0c806da511753887cef823e2f3b4f0d0
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-30 15:29:54 -0300

    AP4 Concluida

--- ed6_ap4.dart
+++ ed6_ap4.dart
@@ -1,1 +1,35 @@
-void main() {}
+void main() {
+  try {
+    final arquivo = ArquivoTexto("aby.txt");
+    arquivo.abrir();
+  } catch (e) {
+    print(e);
+  } finally {
+    print("Fim do programa.");
+  }
+}
+
+abstract class Arquivo {
+  void abrir();
+}
+
+class ArquivoTexto implements Arquivo {
+  ArquivoTexto(this.nome) {
+    if (nome.contains('y')) {
+      throw Exception("Entrada invalida. Digite um nome valido.");
+    }
+  }
+
+  final String nome;
+
+  @override
+  void abrir() {
+    try {
+      !nome.contains('.')
+          ? print("Arquivo aberto com sucesso")
+          : throw Exception("Erro ao abrir o arquivo $nome");
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
